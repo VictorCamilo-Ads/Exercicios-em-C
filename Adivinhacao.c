bef0e5ba3f7bcf9fc5f9fc5bec373778294f2c0a
Victor Camilo <victor_camilo66@hotmail.com>
2026-09-08 23:42:03 -0300

    codando um jogo

---
+++ Adivinhacao.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("******************************************\n");
+    printf("* BEM VINDO AO NOSSO JOGO DE ADIVINHAÇÃO *\n");
+    printf("******************************************\n");
+
+
+}
