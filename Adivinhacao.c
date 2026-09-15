abf5c09b511c41a63456d2630eb20af7b98a57c6
VictorCamilo-Ads <victor_camilo66@hotmail.com>
2026-09-14 21:32:07 -0300

    Atualiza jogo de adivinhacao

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -5,6 +5,7 @@ int main(){
     printf("******************************************\n");
     printf("* BEM VINDO AO NOSSO JOGO DE ADIVINHACAO *\n");
     printf("******************************************\n");
+    printf("Testando Git Msys");
 
 
 }
