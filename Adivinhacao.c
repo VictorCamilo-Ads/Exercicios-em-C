2762db8b2e9060340695e4f1b12ebbce90dabf4d
VictorCamilo-Ads <victor_camilo66@hotmail.com>
2026-09-15 21:11:01 -0300

    Atualizando jogo de adivinhacao

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -6,5 +6,21 @@ int main(){
     printf("* BEM VINDO AO NOSSO JOGO DE ADIVINHACAO *\n");
     printf("******************************************\n");
 
+int numerosecreto = 42;
+
+int chute;
+
+printf("Digite o seu chute: \n");
+scanf("%d", &chute);        
+printf("Seu chute foi: %d\n", chute);
+
+if (chute == numerosecreto) {
+    printf("Parabens! Voce acertou!\n");
+    printf("Voce é um verdadeiro mestre da adivinhação!, jogue novamente!\n");
+} else {
+    printf("Voce errou! mas não desanime, tente novamente!\n");
+}
+
+
 
 }
