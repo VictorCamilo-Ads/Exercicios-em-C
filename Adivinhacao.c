d855720fc24d68ba0e13f1ba5253897d1cac3c44
VictorCamilo-Ads <victor_camilo66@hotmail.com>
2026-09-22 15:30:22 -0300

    bloqueios de numeros negativos

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -12,6 +12,7 @@ int numerosecreto = 42;
 
 int chute;
 
+
 for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
 
     printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
@@ -20,26 +21,36 @@ for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
     scanf("%d", &chute);        
     printf("Seu chute foi: %d\n", chute);
 
+    if (chute < 0) {
+        printf("Voce nao pode chutar numeros negativos!\n");
+        i--;
+        
+        continue;
+    }
+
     int acertou = (chute==numerosecreto);
+    int maior = (chute>numerosecreto);
+    int menor = (chute<numerosecreto);
+
+
 
     if (acertou) {
         printf("Parabens! Voce acertou!\n");
-        printf("Voce é um verdadeiro mestre da adivinhação!, jogue novamente!\n");  
+        printf("Voce e um verdadeiro mestre da adivinhacao!, jogue novamente!\n");  
         
         break;
 
     }
 
-    else {
-        int maior = (chute>numerosecreto);
+    else if (maior) {
 
-        if(maior) {
             printf("Seu chute foi maior que o numero secreto!\n");
-        }
-        else {
-            printf("Seu chute foi menor que o numero secreto!\n");
-        }       
     }
+
+    else {
+            printf("Seu chute foi menor que o numero secreto!\n");
+    }       
+    
 }
     printf("Fim de jogo!\n");
 
